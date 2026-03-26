a69966bec9e60131be598cbaf0aaad5e00abfbb5
BatShinebayar <shinebayar.b@tussolution.mn>
2026-03-27 00:50:47 +0800

    style update and more

--- backend/learnforge/settings.py
+++ backend/learnforge/settings.py
@@ -7,8 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*')
-
+ALLOWED_HOSTS = ['*']
 # ─── APPS ──────────────────────────────────────────────────────────────────
 INSTALLED_APPS = [
     'django.contrib.admin',
